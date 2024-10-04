dff259ee409fcb4d2155809d00a55a3014750259
Gerald Leenerts <gerald.leenerts@gmail.com>
2024-10-04 15:35:05 -0700

    dark mode for explore

--- webapp/app/routes/explore.tsx
+++ webapp/app/routes/explore.tsx
@@ -4,7 +4,7 @@ import { Layout, ContactUsLinks, Footer } from "../components/layout";
 import homeStyles from "../styles/home.css?url";
 import exploreStyles from "../styles/explore.css?url";
 import exampleHome from "../images/explore/example-home.svg";
-import airArrows from "../images/explore/air-arrows.svg";
+import exampleHomeDark from "../images/explore/example-home-dark.svg";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: homeStyles },
@@ -17,7 +17,7 @@ export default function Explore() {
       <div className="explore-bg">
         <div className="mt-20">
           <div className="container mx-auto px-4">
-            <h2 className="text-4xl" style={{ color: "var(--purple-light)" }}>
+            <h2 className="text-4xl explore-title">
               We build a future with nature.
             </h2>
             <p className="mt-4 mb-4 text-lg max-w-lg">
@@ -25,8 +25,10 @@ export default function Explore() {
               into the landscape.
             </p>
             <div className="example-home">
-              <div className="example-home-arrow text-md">Example home</div>
-              <img src={exampleHome} alt="Example Home" />
+              <div className="example-home-arrow text-md">
+                Example home <div className="arrow" />
+              </div>
+              <div className="example-home-img" />
             </div>
           </div>
         </div>
@@ -34,12 +36,7 @@ export default function Explore() {
         <div className="container mx-auto px-4 flex md:justify-end">
           <div className="md:w-1/2">
             <div>
-              <span
-                className="text-2xl"
-                style={{ color: "var(--purple-light)" }}
-              >
-                Simple, eh?
-              </span>
+              <span className="text-2xl explore-title">Simple, eh?</span>
               <p className="mt-4 text-lg max-w-lg">
                 A basic form factor allows us to build with better materials,
                 resulting in a surprisingly comfortable and healthy space.
@@ -47,11 +44,11 @@ export default function Explore() {
             </div>
             <div className="home-size-ranges">
               <div className="sq-ft-range">1,536 - 2,400 sq/ft</div>
-              <div className="width">
+              <div className="width explore-title">
                 32 - 40’
                 <div />
               </div>
-              <div className="depth">
+              <div className="depth explore-title">
                 <div />
                 48 - 60’
               </div>
@@ -60,7 +57,7 @@ export default function Explore() {
         </div>
 
         <div className="mt-20 container mx-auto px-4">
-          <h3 className="text-4xl" style={{ color: "var(--purple-light)" }}>
+          <h3 className="text-4xl explore-title">
             Clean Air, Healthy Building.
           </h3>
         </div>
@@ -76,7 +73,7 @@ export default function Explore() {
         <div className="air-arrows container mx-auto mt-2 px-4" />
         <div className="sm:flex -mt-6 container mx-auto px-4">
           <div className="lg:w-1/3 md:w-1/2 w-full">
-            <h4 className="text-2xl" style={{ color: "var(--green)" }}>
+            <h4 className="text-2xl explore-sub-title">
               Tight Building Envelope
             </h4>
             <ul className="list-disc list-inside">
@@ -85,10 +82,8 @@ export default function Explore() {
               <li>Lower energy consumption</li>
             </ul>
           </div>
-          <div className="lg:w-1/3 md:w-1/2 w-full">
-            <h4 className="text-2xl" style={{ color: "var(--green)" }}>
-              Natural Materials
-            </h4>
+          <div className="lg:w-1/3 md:w-1/2 w-full mt-8 sm:mt-0">
+            <h4 className="text-2xl explore-sub-title">Natural Materials</h4>
             <ul className="list-disc list-inside">
               <li>Lower environmental impact</li>
               <li>Less toxic (lower VOCs)</li>
