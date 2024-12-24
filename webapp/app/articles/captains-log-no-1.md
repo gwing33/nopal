64cb9c3afc2b730a60332e4ca8d15621c62d7f8b
Gerald Leenerts <gerald.leenerts@gmail.com>
2024-12-23 23:30:23 -0700

    Playing around with ideas

---
+++ webapp/app/articles/captains-log-no-1.md
@@ -0,0 +1,70 @@
+# Note No.1
+
+## Capitian Speaking.
+There is a lot to do here. We need to start making things dynamic.
+
+There are 3 main columns at the moment:
+1. Notions *noun*: An individual's conception or impression of something known, experienced, or imagined
+2. Markdown *markup*: Lightweight markup language.
+3. Annotatings *verb*: to make or furnish critical or explanatory notes or comment.
+
+These are all parts of our speech. We can use these 3 things to help us navigate the world.
+If we need more than these, then we need to ask ourselves, what am I trying to do?
+
+### The Rule of 3 Pillars
+And we swear, that the 3 pillars are all we need.
+We need no more and should correct our trajectory to maintain the ethos of these 3 pillars.
+This is why we make an oath that these be always observed:
+- responsive
+- zoom in/out
+- middle column limits page height
+
+Ramen 🍜
+
+### Imagine
+Column isn't really the right word.
+The traditional layout is in 3 columns.
+However, let's say you want a photo album the *annotatings* could be betwixt the photos.
+
+### No.tebook System
+But yes, we do need a system. This one becomes critical to the whole operation.
+But it's a simple one.
+When you format a markdown title like this, `Space aAzZ No.1`, you can think of this as adding a notion to your notebook.
+In fact, sometimes I label a physical notebook on the binding with the same title.
+
+Now this can be linked from other notions.
+For example, `Space aAzZ No.1: A Title` can be linked by doing the following `[Space aAzZ No.1](#space-aazz-no1)`.
+Eventually it'd be good to have an autocomplete for this.
+Like `[S` would bring up all `No.` tags that start with `S`, like `Space sSsS No.3`.
+
+Now...you could give your notions more sensible names like `Note No.1` or `Thought No.73: What aboot a version?` or you could even give them duplicate name: `Thought No.73: Oh, version 2 is here.`.
+You're simply creating a version.
+
+But what if you didn't mean to version something?
+No worries, just number it different and it'll de-version it.
+
+What if I don't want to give it a number? It's just an uncooked thought?
+That's fine too, simply give it a question mark instead of a number: `Thought No.?`.
+This is treated as an uncooked thought.
+Uncooked thoughts are good for simple ideas, see [Uncooked Thoughts](#to-do-uncooked-thoughts) for more.
+
+
+## Access
+Eventually it'd be great to give someone, group or make public your notations. They would be static views, if you wanted to edit you simply checkout the notion and offer up an edit or turn it into something different.
+
+Everything is moderation because everything is owned.
+
+## Style
+If we allow SVGs to be background or annotatings, this could be a level of personalization that is unmatched. Simply copy SVG from Figma or similar tool and paste it in.
+
+## Over and out.
+At the end of each page, we should look to see if there are any additional tasks to be completed for this document.
+For this entry, it seems like these are ideas I'd like to explore.
+- [x] Imagine
+- [ ] Create #uncooked-thoughts-todo (auto generate from above)
+- [ ] Link to other pages
+- [ ] Tasking by MD
+- [ ] Quick find
+- [x] Numero System
+- [ ] Refine the Notions column.
+- [x] Access
---
+++ webapp/app/routes/mrgnt.tsx
@@ -0,0 +1,94 @@
+import mrgntStyles from "../styles/mrgnt.css?url";
+import { LinksFunction } from "@remix-run/node";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: mrgntStyles },
+];
+
+export default function Mrgnt() {
+  return (
+    <>
+      <div className="mdid">MRGNT No.0</div>
+      <div className="notions">
+        <div>
+          <div className="notions-box">
+            <ul>
+              <li>
+                <a className="link">+Presentation</a>
+              </li>
+              <li>
+                <a className="link text-nowrap">+Newpaper Clippings</a>
+              </li>
+              <li>
+                <a className="link">+Print</a>
+              </li>
+              <li>
+                <a className="link">+View-Master Reel</a>
+              </li>
+              <li>
+                <a className="link">+Betamax</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="md-content">
+          <div className="mdmdmdmdmdmdmdmd">
+            A lot of goals, clocks rewind. Some fall to action, others need
+            time. Below a list, thoughts of mind. Enjoy the process, as will
+            mine.
+            <h1># Goal</h1>
+            <p>
+              The purpose of this site is singularly focused on organization.
+              How can we better organize our thoughts, process, tasks, or
+              documentation? How can we organize these such that others get
+              looped in at the right time? How can it be simple and quick no
+              matter the modality?
+            </p>
+            <h2>## Organization</h2>
+            <p>
+              Be militant on organization. When we organize our thoughts, we
+              naturally communicate to others in a way that inspires.
+            </p>
+            <p>
+              Think of it this way, when you organize your thoughts for others,
+              you change the way you talk. One of telling someone facts vs one
+              that is sharing a story.
+            </p>
+            <p>
+              Facts are simply part of a story, they are needed to make the
+              story.
+            </p>
+            <h2>## Story</h2>
+            <p>
+              The nice thing about story is we can communicate this in multiple
+              different formats. Typing, images, video, audio, or doodles on a
+              paper. These things all lend small piece of the overall story.
+            </p>
+            <h2>## Fin</h2>
+            <p>
+              In the end, the more thoughts we can capture and organize, the
+              more interesting our story is.
+            </p>
+          </div>
+          <div className="annotations">
+            <div className="annoiting" style={{ marginTop: "20%" }}>
+              I should be able to add this annotation whever I like!
+            </div>
+            <div
+              className="annoiting"
+              style={{
+                marginTop: "40%",
+              }}
+            >
+              But I should be limited, because in the real world a paper is only
+              so big. So you can't just keep on writing as you run out of
+              physical space. It's absurd quite frankly. If you have a thought
+              that takes up an entire page, fine. Turn. The. Page. Seriously.
+              Turn it.
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
