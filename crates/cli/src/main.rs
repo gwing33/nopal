258d92dca7b7c8a291b4b316337cc8db8090d49e
Gerald Leenerts <gerald.leenerts@gmail.com>
2025-01-09 15:06:48 -0700

    Write load cell readings to file

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,8 +1,15 @@
 use clap::{Parser, Subcommand}; // Args, ValueEnum
+use jiff;
+use serialport::{SerialPort, SerialPortType};
+use std::fs::File;
+use std::io::Write;
+use std::io::{self, Read};
+use std::time::Duration;
 
 #[derive(Debug, Subcommand)]
 enum Command {
     Test {},
+    RecordLoadCell {},
 }
 
 #[derive(Debug, Parser)]
@@ -19,5 +26,70 @@ fn main() {
         Command::Test {} => {
             println!("Testing");
         }
+        Command::RecordLoadCell {} => {
+            println!("Recording load cell info");
+            if let Err(e) = read_serial_port("DK0HR7JK") {
+                eprintln!("Error: {}", e);
+            }
+        }
     }
+}
+
+fn read_serial_port(port_name: &str) -> Result<(), Box<dyn std::error::Error>> {
+    // List available ports
+    let ports = serialport::available_ports()?;
+    let mut port: Option<&serialport::SerialPortInfo> = None;
+
+    // Find and print available ports
+    println!("Available ports:");
+    for p in &ports {
+        if p.port_name.contains(port_name) {
+            port = Some(p);
+        }
+    }
+
+    match port {
+        Some(p) => {
+            println!("{}", p.port_name);
+            if let SerialPortType::UsbPort(info) = &p.port_type {
+                println!("  VID:{:04x} PID:{:04x}", info.vid, info.pid);
+            }
+
+            let mut port = serialport::new(p.port_name.clone(), 2_000_000)
+                .timeout(Duration::from_millis(10))
+                .open()?;
+
+            let now = jiff::Timestamp::now();
+            let filename = format!(
+                "./data/load_cell_data_{}.txt",
+                now.strftime("%Y-%m-%d-%H:%M").to_string()
+            );
+            let mut file = File::create(&filename)?;
+            let mut serial_buf: Vec<u8> = vec![0; 1000];
+
+            println!("Recording data (Ctrl+C to stop)...");
+
+            loop {
+                match port.read(serial_buf.as_mut_slice()) {
+                    Ok(t) => {
+                        let data = String::from_utf8_lossy(&serial_buf[..t]);
+                        file.write_all(data.as_bytes())?;
+                        file.flush()?;
+                        print!("{}", data);
+                    }
+                    Err(ref e) if e.kind() == io::ErrorKind::TimedOut => continue,
+                    Err(e) => {
+                        eprintln!("Error: {}", e);
+                        break;
+                    }
+                }
+            }
+        }
+        None => {
+            eprintln!("Error: Port not found");
+            return Ok(());
+        }
+    }
+
+    Ok(())
 }
