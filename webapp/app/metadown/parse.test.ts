13ebb100268dee62909c76838a7446a0d60e6ed3
Gerald Leenerts <gerald.leenerts@gmail.com>
2025-05-01 19:47:41 -0700

    remove unused files

--- webapp/app/metadown/parse.test.ts
+++
@@ -1,6 +0,0 @@
-import { parse } from "./parse";
-
-test("parse metadata out of markdown", () => {
-  expect(parse(`# Simple Journey`)).toEqual({ id: null, type: null });
-  expect(parse(`# Note No.1: Journey`)).toEqual({ id: "1", type: "Note" });
-});
